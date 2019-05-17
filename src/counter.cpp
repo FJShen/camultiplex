c71ad2dd5f89727e06fd2f4aed543e8fffa02d12
FJShen <asfrga@outlook.com>
2019-05-17 16:50:14 -0400

    created frame counter class

---
+++ src/counter.cpp
@@ -0,0 +1,30 @@
+#include "camultiplex/helper.h"
+
+namespace helper{
+
+    void counter::updateSeq(unsigned long value){
+	if(first_time){
+	    current_seq = value;
+	    first_time = false;
+	    return;
+	}
+	else{
+	    previous_seq = current_seq;
+	    current_seq = value;
+	    loss += (current_seq - previous_seq -1);
+	    return;
+	}
+    }
+
+    unsigned long counter::getLoss(){
+	return loss;
+    }
+
+    unsigned long counter::getCurrentSeq(){
+	return current_seq;
+    }
+
+
+
+
+}
